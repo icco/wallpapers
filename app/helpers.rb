62e81d285817a044cbf303573ba1389b791f0a64
Nat Welch <nat@natwelch.com>
2012-10-16 23:49:26 -0700

    whhoops.

--- app/helpers.rb
+++ app/helpers.rb
@@ -11,8 +11,8 @@ Wallpapers.helpers do
     else
       credentials = {
         :provider                         => 'Google',
-        :google_storage_access_key_id     => GOOGLE_KEY,
-        :google_storage_secret_access_key => GOOGLE_SECRET,
+        :google_storage_access_key_id     => ENV['GOOGLE_KEY'],
+        :google_storage_secret_access_key => ENV['GOOGLE_SECRET'],
       }
     end
 
