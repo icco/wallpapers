34b5e3b2580fb442538056a746644ca209503358
Nat Welch <nat@natwelch.com>
2015-04-20 21:25:09 +0000

    delete boilerplate

--- app/app.rb
+++
@@ -1,7 +0,0 @@
-class Wallpapers < Padrino::Application
-  register Padrino::Rendering
-  register Padrino::Helpers
-  register Padrino::Cache
-  enable :caching
-  set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
-end
