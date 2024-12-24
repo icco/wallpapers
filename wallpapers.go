975bb0a22082a09f8e31b82626094886f2a098c4
Nat Welch <nat@natwelch.com>
2024-12-24 00:39:41 +0000

    lint

--- wallpapers.go
+++ wallpapers.go
@@ -3,6 +3,7 @@ package wallpapers
 import (
 	"cmp"
 	"context"
+	"errors"
 	"fmt"
 	"hash/crc32"
 	"path/filepath"
@@ -44,12 +45,14 @@ func GetGoogleCRC(ctx context.Context, filename string) (uint32, error) {
 	}
 
 	attr, err := client.Bucket(Bucket).Object(filename).Attrs(ctx)
-	if err != nil && err != storage.ErrObjectNotExist {
-		return 0, fmt.Errorf("could not get attrs: %w", err)
-	}
+	if err != nil {
+		if !errors.Is(err, storage.ErrObjectNotExist) {
+			return 0, fmt.Errorf("could not get attrs: %w", err)
+		}
 
-	if err == storage.ErrObjectNotExist {
-		return 0, nil
+		if errors.Is(err, storage.ErrObjectNotExist) {
+			return 0, nil
+		}
 	}
 
 	return attr.CRC32C, nil
